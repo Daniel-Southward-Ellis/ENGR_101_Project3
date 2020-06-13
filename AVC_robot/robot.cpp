e425ad9c0e257dca363f254547b24fb2b518c17f
Daniel <snakefangox@gmail.com>
2020-06-14 11:47:26 +1200

    Switched to an average based line following system

--- AVC_robot/robot.cpp
+++ AVC_robot/robot.cpp
@@ -1,8 +1,8 @@
 
 #include "robot.hpp"
 
-#define WHITE_THRESHOLD 240 // threshold for something being 'white'
-#define CENTRE_OFFSET 10 // allow some offset, accounting for scenarios where we may not make the right analysis of pixel pos
+#define WHITE_THRESHOLD 250 // threshold for something being 'white'
+#define CENTRE_OFFSET 2 // allow some offset, accounting for scenarios where we may not make the right analysis of pixel pos
 #define LINE_WIDTH 4 // it appears that the line takes up 4 pixels (using output to make that assumption)
 /**
  * Analyse image, and determine whether there is enough 'straight' pixels within the robot's centre
@@ -19,14 +19,13 @@
  * @return 0 for straight move, 1 for left move, 2 for right move, -1 for failure (never condition)
  */
 int analyse_image(ImagePPM image) {
-	int robot_centre_view = image.width/2; // we want the line to fall within a range of the robots centre view
+	int robot_centre_view = (image.width/2); // we want the line to fall within a range of the robots centre view
 	// testing statements
 	printf("CENTRE OF ROBOTS VIEW: %i\n", image.width/2);
 	// var setting
-	int num_straight_pixels = 0; // total num straight pixels (recorded throughout offset)
-	int max_centre_offset = 0; // current max pixel offset from centre
-	int col_of_offset = -1; // column of pixel that currently has largest offset from centre
-	int start_row = ((5*(image.height))/6)+10; // row that the analysis should start from
+	int num_white_pixels = 0; // total num white pixels
+	int avg_offset = 0; // average white pixel offset from centre
+	int start_row = 0; // row that the analysis should start from
 	// iterate through all the rows and columns in the image, get each pixel
 	for (int row = start_row; row < image.height; row++) {
 		for (int column = 0; column < image.width; column++) {
@@ -37,37 +36,29 @@ int analyse_image(ImagePPM image) {
 			int luminocity = get_pixel(image, row, column, 3);
 			if (red >= WHITE_THRESHOLD && green >= WHITE_THRESHOLD && blue >= WHITE_THRESHOLD && luminocity >= WHITE_THRESHOLD) {
 				// have found white pixel, note: white threshold value is just a test, may need to change
-				if (column >= (robot_centre_view - CENTRE_OFFSET) && column <= (robot_centre_view + CENTRE_OFFSET)) {
-					// we have found a centre pixel
-					num_straight_pixels++;
-				}
-				else {
-					int pixel_centre_offset = robot_centre_view - column; // calculate offset from CENTRE
-					if (pixel_centre_offset < 0 ) { pixel_centre_offset *= -1; } // getting abs. values (non neg)
-					// if current pixel has a higher offset than current max (from centre), set it as new max offset
-					if (pixel_centre_offset > max_centre_offset) {
-						max_centre_offset = pixel_centre_offset;
-						col_of_offset = column;
-					}
-				}
+				avg_offset += column - robot_centre_view;
+				++num_white_pixels;
 			}
 		}
 	}
+	if (num_white_pixels > 0){
+		avg_offset = avg_offset / num_white_pixels;
+	}
 	// test stmts
-	printf("num straight pixels: %i\n", num_straight_pixels/LINE_WIDTH);
+	printf("average pixel offset: %i\n", avg_offset);
 	printf("num rows analysed: %i\n", image.height-start_row);
 	// for analysis to consider line straight, all pixels straight ahead must be within centre vision
-	if (((num_straight_pixels/LINE_WIDTH)) >= (image.height-start_row)) {
+	if (avg_offset >= -CENTRE_OFFSET && avg_offset <= CENTRE_OFFSET) {
 		printf("robot should move straight\n");
 		return 0; // indicates robot should move straight
 	}
 	// if the column of offset is larger than the robots centre view, robot should turn left
-	else if (col_of_offset != -1 && col_of_offset > robot_centre_view){
+	else if (avg_offset < -CENTRE_OFFSET) {
 		printf("robot should turn left\n");
 		return 1;
 	}
 	// if the column of offset is smaller than the robots centre view, robot should turn right
-	else if (col_of_offset < robot_centre_view) {
+	else if (avg_offset > CENTRE_OFFSET) {
 		printf("robot should turn right\n");
 		return 2;
 	}
@@ -88,9 +79,8 @@ int main(){
     while(1){
 			ImagePPM image;
 			takePicture();
-	    SavePPMFile("i0.ppm",cameraView);
-			OpenPPMFile("i0.ppm", image);
-			int direction = analyse_image(image);
+	    	SavePPMFile("i0.ppm",cameraView);
+			int direction = analyse_image(cameraView);
 			if (!direction) { // if direction == 0, move straight
 					rotation_count_left = 0;
 					rotation_count_right = 0;
@@ -98,16 +88,16 @@ int main(){
 					vRight = 10.0;
 			}
 			else if (direction == 1) { // spin left until we can move straight!
-				rotation_count_right = 0;
-				rotation_count_left++;
-				vLeft = 10.0;
-				vRight = 0.0;
-			}
-			else if (direction == 2) {
 				rotation_count_left = 0;
 				rotation_count_right++;
 				vLeft = 0.0;
 				vRight = 10.0;
+			}
+			else if (direction == 2) {
+				rotation_count_right = 0;
+				rotation_count_left++;
+				vLeft = 10.0;
+				vRight = 0.0;
 			}
       setMotors(vLeft,vRight);
       std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
